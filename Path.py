01644dffc0b6b644402aa24bbe1f5db6a0cf1d77
nidhijoshi123 <nidhi.nj.joshi@fau.de>
2019-06-25 23:27:47 +0200

    Update Path.py

--- Path.py
+++ Path.py
@@ -5,15 +5,15 @@ Created on Wed Jan 30 18:48:15 2019
 @author: nidhi
 """
 
-cancerPath = 'C:/Users/Kishore/Pictures/Cancerous/'
-normalPath = 'C:/Users/Kishore/Pictures/Normal/'
-cancerTrainPath = 'C:/Users/Kishore/Pictures/Taining/Cancerous/'
-cancerValidationPath = 'C:/Users/Kishore/Pictures/Validation/Cancerous/'
-normalTrainPath = 'C:/Users/Kishore/Pictures/Taining/Normal/'
-normalValidationPath = 'C:/Users/Kishore/Pictures/Validation/Normal/'
-trainPath = 'C:/Users/Kishore/Pictures/Taining/'
-validationPath = 'C:/Users/Kishore/Pictures/Validation/'
-testPath = 'C:/Users/Kishore/Pictures/Test/'
-cancerTestPath = 'C:/Users/Kishore/Pictures/Test/Cancerous/'
-normalTestPath = 'C:/Users/Kishore/Pictures/Test/Normal/'
+cancerPath = '<Base path>/Cancerous/'
+normalPath = '<Base path>/Normal/'
+cancerTrainPath = '<Base path>/Taining/Cancerous/'
+cancerValidationPath = '<Base path>/Validation/Cancerous/'
+normalTrainPath = '<Base path>/Taining/Normal/'
+normalValidationPath = '<Base path>/Validation/Normal/'
+trainPath = '<Base path>/Taining/'
+validationPath = '<Base path>/Validation/'
+testPath = '<Base path>/Test/'
+cancerTestPath = '<Base path>/Test/Cancerous/'
+normalTestPath = '<Base path>/Test/Normal/'
 numberOfImages = 1292
